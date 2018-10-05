5827c27ab37314ae383095194325557f57ce810c
Florian Lorétan <florian.loretan@wunder.io>
2018-10-05 09:15:09 +0200

    Add an equivalent starting point for our string wrapping kata in javascript.

---
+++ js/test/StringWrapperTest.js
@@ -0,0 +1,23 @@
+var assert = require('assert');
+
+/**
+ * Takes in a string and adds new lines as needed
+ * to wrap the text so that a single line never
+ * exceeds $length.
+ *
+ * @param string
+ * @param length
+ * @returns string
+ */
+function wrapStrings(string, length) {
+  return string;
+}
+
+describe('StringWrapper', function() {
+  it('should wrap short strings', function() {
+    const input = 'a';
+    const output = wrapStrings(input, 3);
+    assert.equal('a', output);
+  });
+
+});
